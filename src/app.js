0057578a3e9a82bc3bb3f7c694900108b3fac488
Hardie-stuff <lara.hardie.stewart@gmail.com>
2021-08-19 13:42:09 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -34,11 +34,18 @@ function formatDay(timestamp) {
 
 
 
-
 function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#temperature")
-    temperatureElement.innerHTML = response.data.main.temp
-    
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#weather-description");
+    let humidityElement = document.querySelector("#humidity");
+    let windELement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windELement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "d158e814af56482efd1df2f3283070e5";
